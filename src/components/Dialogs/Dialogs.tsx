ca7ea8b1e7696aaa73abd15f6efa1c4fb20312af
Aleksvtop <vazilino@gmail.com>
2022-11-20 12:27:35 +0300

    Change Dialogs.tsx&Dialogs.module.css

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,10 +1,32 @@
 import React from 'react'
 import classesDial from './Dialogs.module.css'
+import {NavLink} from "react-router-dom";
 
 export const Dialogs = () => {
     return (
-        <div>
-            Dialogs!
+        <div className={classesDial.dialogs}>
+            <div className={classesDial.dialogsItems}>
+                <div className={classesDial.dialog + ' ' + classesDial.active}>
+                    <NavLink to={'/dialogs/1'}>Aleksey</NavLink>
+                </div>
+                <div className={classesDial.dialog}>
+                    <NavLink to={'/dialogs/2'}>Dmitriy</NavLink>
+                </div>
+                <div className={classesDial.dialog}>
+                    <NavLink to={'/dialogs/3'}>Evgeniy</NavLink>
+                </div>
+                <div className={classesDial.dialog}>
+                    <NavLink to={'/dialogs/4'}>Olga</NavLink>
+                </div>
+                <div className={classesDial.dialog}>
+                    <NavLink to={'/dialogs/5'}>Valera</NavLink>
+                </div>
+            </div>
+            <div className={classesDial.messages}>
+                <div className={classesDial.message}>Hi</div>
+                <div className={classesDial.message}>How are you</div>
+                <div className={classesDial.message}>Yo</div>
+            </div>
         </div>
     )
 }
